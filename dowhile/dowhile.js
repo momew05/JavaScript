a1e6cabde599582ff34cd79bcd8865a6b3b75a2c
mewo <kornenko_liza@mail.ru>
2026-02-06 14:21:01 +1000

    do/while

---
+++ dowhile/dowhile.js
@@ -0,0 +1,36 @@
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+let sum = 0, num = 1;
+while (num <= 10) {
+    sum += num;
+    num++;
+}
+console.log('сумма:', sum); 
+
+let n = 0;
+while (n < 10) {
+    if (n % 2 === 0) console.log(n);
+    n++;
+}
+
+let guess = 0;
+do {
+    guess = Math.floor(Math.random() * 3) + 1;
+    console.log('Угадал:', guess);
+} while (guess !== 2);
+
+let chars = 'abc123';
+let randomString = '';
+
+do {
+    let randomIndex = Math.floor(Math.random() * chars.length);
+    randomString += chars[randomIndex];
+    console.log('Текущая строка: ' + randomString);
+} while (randomString.length < 5);
+
+console.log('Готовая строка: ' + randomString);
+
